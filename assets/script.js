359246ac212fdac6cc9e14403db87fd84e3bb76e
amarchive <antoine.marchive@orange.fr>
2024-05-23 08:57:46 +0200

    create carousel

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,18 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
+		"image":"/assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"/assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"/assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"/assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
@@ -21,5 +21,37 @@ const slides = [
 const arrowLeft = document.querySelector(".arrow_left");
 const arrowRight = document.querySelector(".arrow_right");
 
-console.log(arrowLeft);
-console.log(arrowRight);
+const bannerImg = document.querySelector(".banner-img");
+const textInfo = document.querySelector (".text-info");
+const dots = document.querySelector (".dots");
+
+let slidePosition = 0;
+
+
+
+// eventlistner pour la gauche qui fait appelle à la fonction changeDirection et en lui injectant le parametre "left"
+	arrowLeft.addEventListener("click", () => {
+	slidePosition --;
+	if (slidePosition <0) {
+		slidePosition = slides.length-1;
+	}
+	changeSlide();
+	console.log("gauche ok") 
+	console.log(slidePosition)
+  });
+
+  // eventlistner pour la droite qui fait appelle à la fonction changeDirection et en lui injectant un parametre autre que 'left'
+  	arrowRight.addEventListener("click", () => {
+	slidePosition ++;
+	if (slidePosition > slides.length-1) {
+		slidePosition = 0;
+	}
+	changeSlide();
+	console.log("droite ok") 
+	console.log(slidePosition)
+  });
+
+  function changeSlide () {
+	bannerImg.src = slides[slidePosition].image;
+	textInfo.innerHTML = slides[slidePosition].tagLine;
+  }
